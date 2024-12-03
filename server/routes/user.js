48313b163fb666916e63c2bf20693f6d2592e395
Yaroslava <slnegrich@gmail.com>
2024-12-03 16:32:57 +0200

    validation fix

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const userController = require('../controllers/user');
-const body = require('express-validation');
+const { body } = require('express-validator');
 
 const router = express.Router();
 
