382ef1e039a1abe7635221e70631436569e9417e
Yaroslava <slnegrich@gmail.com>
2024-12-18 18:53:45 +0200

    recent transactions component

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
-import { Menu } from '../components/Menu/Menu';
-import { MainLayout } from '../styles';
 import { Chart } from '../components/Chart/Chart';
 import { AmountBadge } from '../components/AmountBadge/AmountBadge';
 import { AppDispatch, RootState } from '../store';
@@ -9,30 +7,64 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getToken } from '../utils/localStorageManipulation';
 import { fetchExpensesData } from '../store/expense-slice';
 import { fetchIncomeData } from '../store/income-slice';
+import { RecentTransaction } from '../components/RecentTransactions/RecentTransaction';
 
 const DashboardStyled = styled.div`
     background-color: var(--color-dark-gray);
     height: 100%;
     padding: 20px;
 
-    .expenses-chart {
-      color: white;
-      text-align: center;
-    }
-
     .badges {
       display: flex;
-      gap: 1rem;
-      justify-content: space-between;
-      margin-bottom: 1.2rem;
+      gap: 1.2rem;
+      margin-bottom: 3rem;
     }
 
+    .dashboard-footer {
+      display: flex;
+      color: white;
+      justify-content: space-evenly;
+    }
 
+    .expenses-chart {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      /* justify-items: center; */
+      gap: 1rem;
+    }
+
+    .recent-transactions {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      /* justify-items: center; */
+      gap: 1rem;
+    }
 
 `
+
+export interface Transaction {
+  _id: string;
+  description: string;
+  amount: number;
+  category: string;
+  date: string | Date;
+  type: "income" | "expense";
+}
+
 export const Dashboard: React.FC = () => {
   const {expenses} = useSelector((state:RootState)=> state.expense)
   const {incomes} = useSelector((state:RootState)=> state.income)
+  const expenseTransactions: Transaction[] = expenses.map(exp=>{
+    return {...exp, type: 'expense'}
+  }) 
+  const incomeTransactions: Transaction[] = incomes.map(inc=>{
+    return {...inc, type: 'income'}
+  }) 
+  const allTransactions = expenseTransactions.concat(incomeTransactions)
+  const sortedTransactions = allTransactions.sort((a,b)=>new Date(b.date).getTime()-new Date(a.date).getTime())
+
   const token = getToken()
   const dispatch = useDispatch<AppDispatch>()
 
@@ -55,11 +87,18 @@ export const Dashboard: React.FC = () => {
         <AmountBadge title='Expenses:' amount={expensesSum}></AmountBadge>
       </div>
       
-      <div className='expenses-chart'>
+      <div className='dashboard-footer'>
+        <div className='expenses-chart'>
         <h4>Expenses Categories</h4>
         <Chart></Chart>
       </div>
-      
+
+      <div className='recent-transactions'>
+        <h4>Recent transacions</h4>
+        {sortedTransactions.slice(0,5).map(transaction=><RecentTransaction transaction={transaction}></RecentTransaction>)}
+      </div>
+      </div>
+    
     </DashboardStyled>
     
   )
